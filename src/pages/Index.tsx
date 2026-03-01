2a4a2f89623513c8190ec7d7535331b13a9e5d78
tem1558 <shraddhasahu.acad@gmail.com>
2026-03-01 16:40:40 +0000

    update

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,7 +6,7 @@ import ProjectsSection from "@/components/portfolio/ProjectsSection";
 import AchievementsSection from "@/components/portfolio/AchievementsSection";
 import NewspaperSection from "@/components/portfolio/NewspaperSection";
 import CommunitySection from "@/components/portfolio/CommunitySection";
-import SkillsSection from "@/components/portfolio/SkillsSection";
+// import SkillsSection from "@/components/portfolio/SkillsSection";
 import Footer from "@/components/portfolio/Footer";
 
 const Index = () => {
@@ -14,7 +14,6 @@ const Index = () => {
     <div className="bg-background text-foreground min-h-screen">
       <Navbar />
       <Hero />
-      <SkillsSection />
       <ExperienceSection />
       <PublicationsSection />
       <ProjectsSection />
